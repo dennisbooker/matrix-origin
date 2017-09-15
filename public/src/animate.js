8fd3b53e2fd8163a062c405179a27642443d8113
markus636 <markus636@mail.com>
2017-09-15 18:25:30 +0200

    First Commit

---
+++ public/src/animate.js
@@ -0,0 +1,20 @@
+var game = new Phaser.Game(500, 500, Phaser.AUTO, 'phaser-example', { preload: preload, create: create });
+
+function preload() {
+    game.load.atlasJSONHash('matrix', 'assets/_IMG0001.png', 'assets/_IMG0001.json');
+}
+
+function create() {
+//BOT
+    //  This sprite is using a texture atlas for all of its animation data
+    var bot = game.add.sprite(200, 200, 'matrix');
+
+    //  Here we add a new animation called 'run'
+    //  We haven't specified any frames because it's using every frame in the texture atlas
+    bot.animations.add('run');
+
+    //  And this starts the animation playing by using its key ("run")
+    //  15 is the frame rate (15fps)
+    //  true means it will loop when it finishes
+    bot.animations.play('run', 15, true);
+}
